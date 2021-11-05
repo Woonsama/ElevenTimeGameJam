4c8c3ab97c5954159fad3006c889432b0a503c54
Woonsama <kws4510@naver.com>
2021-11-05 21:32:36 +0900

    [Feat] UIViewBase 추가

---
+++ ElevenGameJamProject/Assets/Scripts/Hotbar/UI/Base/UIViewBase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Hotbar.UI
+{
+    public class UIViewBase : MonoBehaviour
+    {
+        public virtual async Task InitView()
+        {
+            
+        }
+
+        public virtual async Task UpdateView()
+        {
+            
+        }
+    }
+}
