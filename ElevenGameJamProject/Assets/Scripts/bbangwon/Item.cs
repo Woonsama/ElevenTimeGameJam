ea08e78a2bc4055d404762f142c1657f04f28c8a
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2021-11-05 23:29:42 +0900

    배경 ART 추가

---
+++ ElevenGameJamProject/Assets/Scripts/bbangwon/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace eleven.game
+{
+    public class Item : MonoBehaviour
+    {        
+        public int Score;
+
+        private void Start()
+        {
+            tag = "Item";
+        }
+    }
+}
