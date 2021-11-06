e08e822c6e0caae052379029ae110a13d5a98435
Woonsama <kws4510@naver.com>
2021-11-06 22:29:34 +0900

    [Fix] IngameManager 프레임워크 수정

--- ElevenGameJamProject/Assets/Scripts/Hotbar/Manager/InGameManager.cs
+++ ElevenGameJamProject/Assets/Scripts/Hotbar/Manager/InGameManager.cs
@@ -1,8 +1,10 @@
 using Hotbar.UI;
 using Hotbar.UI.View;
 using Hotbar.UI.View.Result;
+using Hotbar.UI.View.Roulette;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Tang3.Common.Management;
 using UnityEngine;
 
@@ -14,38 +16,55 @@ public class InGameManager : SingletonMonoBase<InGameManager>
     {
         await uiHeaderView.InitView();
 
-        ////게임 클리어 팝업 띄우기
-        //var clearView = await UIManager.Instance.OpenView(UIManager.ViewType.ClearResult);
-        //await ((UIResultClearView)clearView).Show(500, (name) =>
-        // {
-        //     Debug.Log("Replay Button Click");
-        // }, () =>
-        // {
-        //     Debug.Log("Back Button Click");
-        // });
-
         ////룰렛 팝업 띄우기
-        //var rouletteView = await UIManager.Instance.OpenView(UIManager.ViewType.Roulette);
-        //rouletteView.Close();
+        var rouletteView = await UIManager.Instance.OpenView(UIManager.ViewType.Roulette);
+        var mode = await ((UIRouletteView)rouletteView).StartSpin();
 
-        ////타이머 시작하기
-        //TimeManager.Instance.StartTimer(11, async () =>
-        //{
-        //    //시간 초과 팝업 띄우기
-        //    var failView = await UIManager.Instance.OpenView(UIManager.ViewType.FailResult);
-        //    await ((UIResultFailView)failView).Show(500, () =>
-        //    {
-        //        Debug.Log("Replay Button Click");
-        //    }, () =>
-        //    {
-        //        Debug.Log("Back Button Click");
-        //    });
-        //});
-        //uiHeaderView.SetRemainPlayTime();
+        //받아온 난이도로 게임 설정
+
+        rouletteView.Close();
+
+        //타이머 시작하기
+        TimeManager.Instance.StartTimer(11, async () =>
+        {
+            await OpenGameFailView();
+        });
+
+        uiHeaderView.SetRemainPlayTime();
+
+        //인게임 시작
     }
 
     private void Update()
     {
         uiHeaderView.RefreshRemainPlayTime();
     }
+
+    #region Event
+
+    public async Task OpenGameClearView()
+    {
+        var clearView = await UIManager.Instance.OpenView(UIManager.ViewType.ClearResult);
+        await ((UIResultClearView)clearView).Show(500, (name) =>
+        {
+            Debug.Log("Replay Button Click");
+        }, () =>
+        {
+            Debug.Log("Back Button Click");
+        });
+    }
+
+    public async Task OpenGameFailView()
+    {
+        var failView = await UIManager.Instance.OpenView(UIManager.ViewType.FailResult);
+        await ((UIResultFailView)failView).Show(500, () =>
+        {
+            Debug.Log("Replay Button Click");
+        }, () =>
+        {
+            Debug.Log("Back Button Click");
+        });
+    }
+
+    #endregion
 }
