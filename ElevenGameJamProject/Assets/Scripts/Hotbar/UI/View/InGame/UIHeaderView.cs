46eeaefaf3f97d18b1b8a05d66e6e153bd10126c
Woonsama <kws4510@naver.com>
2021-11-06 10:15:39 +0900

    [Feat] UIHeaderView 임시 추가

---
+++ ElevenGameJamProject/Assets/Scripts/Hotbar/UI/View/InGame/UIHeaderView.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+namespace Hotbar.UI.View
+{
+    public class UIHeaderView : UIViewBase
+    {
+        public Text remainPlayTimeText;
+        public Text scoreText;
+        public Button settingButton;
+
+        public override async Task InitView()
+        {
+            
+        }
+    }
+}
