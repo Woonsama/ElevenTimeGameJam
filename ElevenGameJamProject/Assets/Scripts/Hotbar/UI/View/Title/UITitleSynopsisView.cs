c945389cec1435c97cec2d0ecc402d32cdd0f78c
pickup335 <pickup335@naver.com>
2021-11-06 23:36:49 +0900

    게임씬 전환

--- ElevenGameJamProject/Assets/Scripts/Hotbar/UI/View/Title/UITitleSynopsisView.cs
+++ ElevenGameJamProject/Assets/Scripts/Hotbar/UI/View/Title/UITitleSynopsisView.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 namespace Hotbar.UI.View.Title
 {
@@ -16,13 +17,15 @@ namespace Hotbar.UI.View.Title
         private void Update()
         {
             cloudImage.position += Vector3.left * Time.deltaTime * 10;
-            sysnopsisText.transform.position += Vector3.up * Time.deltaTime * 30;
+            sysnopsisText.transform.position += Vector3.up * Time.deltaTime * 25;
         }
 
         public override async Task InitView()
         {
             skipButton.onClick?.RemoveAllListeners();
             skipButton.onClick?.AddListener(OnClickSkipButton);
+
+            gameObject.SetActive(true);
         }
 
         public override async Task UpdateView()
@@ -32,7 +35,8 @@ namespace Hotbar.UI.View.Title
 
         private void OnClickSkipButton()
         {
-
+            Debug.Log("T");
+            SceneManager.LoadScene(1);
         }
     }
 
