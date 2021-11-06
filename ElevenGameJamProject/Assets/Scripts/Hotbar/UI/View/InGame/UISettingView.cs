770cedd6e0e93622b40efaad7f9ba779e89df3eb
Woonsama <kws4510@naver.com>
2021-11-06 23:21:59 +0900

    [Feat] UISettingView 작업 중

--- ElevenGameJamProject/Assets/Scripts/Hotbar/UI/View/InGame/UISettingView.cs
+++ ElevenGameJamProject/Assets/Scripts/Hotbar/UI/View/InGame/UISettingView.cs
@@ -10,40 +10,16 @@ namespace Hotbar.UI.View
 {
     public class UISettingView : UIViewBase
     {
-        [Header("Window")]
-        public Dropdown dropdown;
-
         [Header("CloseButton")]
         public Button closeButton;
-
-        private bool isFullScreen;
-        private Vector2 resolution;
 
         public override async Task InitView()
         {
             Time.timeScale = 0;
-            dropdown.onValueChanged?.AddListener(OnValueChangedDropdownCallBack);
             closeButton.onClick?.AddListener(Close);
         }
 
         #region Event
-
-        private void OnValueChangedDropdownCallBack(int index)
-        {
-            switch(index)
-            {
-                case 1: //전체 화면
-                    Screen.SetResolution(Screen.width, Screen.height, true);
-                    isFullScreen = true;
-                    break;
-
-                case 2: //창모드
-                    Screen.SetResolution(Screen.width, Screen.height, false);
-                    isFullScreen = false;
-
-                    break;
-            }
-        }
 
         public override void Close()
         {
