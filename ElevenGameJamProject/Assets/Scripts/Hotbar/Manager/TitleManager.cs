a733fa8962541e5d73ea52d9da5a77d4e21992c5
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2021-11-07 14:15:14 +0900

    모바일 타이틀 씬 해상도 문제

--- ElevenGameJamProject/Assets/Scripts/Hotbar/Manager/TitleManager.cs
+++ ElevenGameJamProject/Assets/Scripts/Hotbar/Manager/TitleManager.cs
@@ -8,5 +8,27 @@ public class TitleManager : SingletonMonoBase<TitleManager>
 {
     public UITitleView uiTitleView;
 
-    private async void Awake() => await uiTitleView.InitView();
+    private async void Awake()
+    {
+#if UNITY_ANDROID
+            int windowModeIndex = PlayerPrefs.GetInt("Window", 0);
+            int resolutionModeIndex = PlayerPrefs.GetInt("Resolution", 0);
+            int resolutionX = 1920, resolutionY = 1080;
+
+            if (windowModeIndex == 0 && resolutionModeIndex == 0)
+            {
+                resolutionX = 1920;
+                resolutionY = 1080;
+            }
+            else if (windowModeIndex == 0 && resolutionModeIndex == 1)
+            {
+                resolutionX = 1280;
+                resolutionY = 720;
+            }
+
+            Screen.SetResolution(resolutionX, resolutionY, true);
+#endif
+
+        await uiTitleView.InitView();
+    }
 }
